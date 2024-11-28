4278b1bbfcafdfa5fa9d477489a0dcc9769559fd
Šimon Brandner <simon.bra.ag@gmail.com>
2024-11-28 11:06:50 +0100

    Add tests
    These were supplied by the RPH team
    
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

---
+++ src/test_simplefilters.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This was written by the RPH team
+"""Tests for simple filters."""
+
+import unittest
+import tst_filterbase
+
+from simplefilters import NaiveFilter, ParanoidFilter, RandomFilter
+
+
+class NaiveFilterTest(tst_filterbase.BaseFilterTestCase):
+
+    def setUp(self):
+        super().setUp()
+        # Set an instance of the NaiveFilter class for the test
+        self.filter = NaiveFilter()
+
+
+class ParanoidFilterTest(tst_filterbase.BaseFilterTestCase):
+
+    def setUp(self):
+        super().setUp()
+        # Set an instance of the ParanoidFilter class for the test
+        self.filter = ParanoidFilter()
+
+
+class RandomFilterTest(tst_filterbase.BaseFilterTestCase):
+
+    def setUp(self):
+        super().setUp()
+        # Set an instance of the RandomFilter class for the test
+        self.filter = RandomFilter()
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
