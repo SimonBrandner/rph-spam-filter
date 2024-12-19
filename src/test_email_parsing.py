2adc96665e72416c25c5fc4fb94549717500f65f
Jiří Král <jirkakral978@protonmail.com>
2024-12-19 20:12:10 +0100

    Rename email_parses.py to email_parser.py

--- src/test_email_parsing.py
+++ src/test_email_parsing.py
@@ -1,6 +1,6 @@
 import unittest
 from corpus import Corpus
-from email_parses import Email
+from email_parser import Email
 
 EMAILS_PATH = './assets/1/'
 EMAIL_BODY_PREVIEW_LENGTH = 100  # Extracted constant for email body truncation length
