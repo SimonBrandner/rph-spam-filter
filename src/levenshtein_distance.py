0f3ded65fd087a6cbb6e2551a99b3e1ba5f5efb1
Jiří Král <jirkakral978@protonmail.com>
2024-12-22 09:23:59 +0100

    Refactor variable names in Levenshtein distance function.

--- src/levenshtein_distance.py
+++ src/levenshtein_distance.py
@@ -7,20 +7,20 @@ def calc_levenshtein_distance(str1: str, str2: str) -> int:
     if not str2:
         return len(str1)
 
-    n = len(str1)
-    m = len(str2)
-    levenshtein_matrix = np.zeros((n + 1, m + 1))
+    len1 = len(str1)
+    len2 = len(str2)
+    levenshtein_matrix = np.zeros((len1 + 1, len2 + 1))
 
-    for i in range(0, n + 1):
-        levenshtein_matrix[i, 0] = i
-    for i in range(0, m + 1):
-        levenshtein_matrix[0, i] = i
+    for i1 in range(0, len1 + 1):
+        levenshtein_matrix[i1, 0] = i1
+    for i2 in range(0, len2 + 1):
+        levenshtein_matrix[0, i2] = i2
 
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            insertion = levenshtein_matrix[i - 1, j] + 1
-            deletion = levenshtein_matrix[i, j - 1] + 1
-            substitution = levenshtein_matrix[i - 1, j - 1] + (1 if str1[i - 1] != str2[j - 1] else 0)
-            levenshtein_matrix[i, j] = min(insertion, deletion, substitution)
+    for i1 in range(1, len1 + 1):
+        for i2 in range(1, len2 + 1):
+            insertion = levenshtein_matrix[i1 - 1, i2] + 1
+            deletion = levenshtein_matrix[i1, i2 - 1] + 1
+            substitution = levenshtein_matrix[i1 - 1, i2 - 1] + (1 if str1[i1 - 1] != str2[i2 - 1] else 0)
+            levenshtein_matrix[i1, i2] = min(insertion, deletion, substitution)
 
-    return levenshtein_matrix[n, m]
+    return levenshtein_matrix[len1, len2]
