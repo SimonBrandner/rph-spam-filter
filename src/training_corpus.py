247423082d23dffde9d9e7857f1e1831b3e3066d
Šimon Brandner <simon.bra.ag@gmail.com>
2024-12-23 15:40:18 +0100

    Implement some of the missing functions
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/training_corpus.py
+++ src/training_corpus.py
@@ -1,19 +1,26 @@
+from typing import Dict
+import os
 from corpus import Corpus
 from quality import TRUTH_FILENAME
+from test_filterbase import HAM_TAG, SPAM_TAG
+from utils import read_classification_from_file
 
 
 class TrainingCorpus(Corpus):
+    classification: Dict[str, str]
+
+    def __init__(self, path: str):
+        super().__init__(path)
+
+        self.classification = read_classification_from_file(
+            os.path.join(path, TRUTH_FILENAME)
+        )
+
     def get_class(self, email_name: str):
-        pass
+        return self.classification[email_name]
 
     def is_ham(self, email_name: str):
-        pass
+        return self.classification[email_name] == HAM_TAG
 
     def is_spam(self, email_name: str):
-        pass
-
-    def spams(self):
-        pass
-
-    def hams(self):
-        pass
+        return self.classification[email_name] == SPAM_TAG
