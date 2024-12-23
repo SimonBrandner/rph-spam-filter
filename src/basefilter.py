2603921bb2d1fd758a1bead9dbc2db74c92bd011
Šimon Brandner <simon.bra.ag@gmail.com>
2024-12-23 09:45:01 +0100

    Add `write_classification_to_file`
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/basefilter.py
+++ src/basefilter.py
@@ -2,6 +2,7 @@ import os
 from abc import abstractmethod
 from typing import Dict
 from quality import PREDICTION_FILENAME
+from utils import write_classification_to_file
 
 
 class BaseFilter:
@@ -14,8 +15,6 @@ class BaseFilter:
         pass
 
     def write_prediction_to_file(self, path: str, predictions: Dict[str, str]):
-        with open(
-            os.path.join(path, PREDICTION_FILENAME), "w", encoding="utf-8"
-        ) as file:
-            for name, prediction in predictions.items():
-                file.write(f"{name} {prediction}\n")
+        write_classification_to_file(
+            os.path.join(path, PREDICTION_FILENAME), predictions
+        )
--- src/utils.py
+++ src/utils.py
@@ -16,3 +16,11 @@ def read_classification_from_file(path: str) -> Dict[str, str]:
             classification_table[email_filename] = classification.strip()
 
     return classification_table
+
+
+def write_classification_to_file(
+    path: str, classification_dictionary: Dict[str, str]
+) -> None:
+    with open(path, "w", encoding="utf-8") as file:
+        for name, classification in classification_dictionary.items():
+            file.write(f"{name} {classification}\n")
